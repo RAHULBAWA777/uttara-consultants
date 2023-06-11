e8239aec04c358b7ed459a26da5f5a3ea2da00ed
RAHULBAWA <rahulbawa514@gmail.com>
2023-06-11 14:46:29 +0530

    Update Sponsors.jsx

--- src/components/MARQUEE/Sponsors.jsx
+++ src/components/MARQUEE/Sponsors.jsx
@@ -8,7 +8,8 @@ const Sponsors = () => {
   
     const imgStyle = {
       width:'20rem',
-      height:'fitContent'
+      height:'fitContent',
+      padding:'2rem'
     }
     const imgStyle2 = {
       width:'15rem',
@@ -23,11 +24,11 @@ const Sponsors = () => {
         <Marquee>
           <img style={imgStyle} src="https://zeevector.com/wp-content/uploads/2021/02/Titan-Logo-PNG.png" alt="" />
           <img style={imgStyle} src="https://download.logo.wine/logo/Bharat_Sanchar_Nigam_Limited/Bharat_Sanchar_Nigam_Limited-Logo.wine.png" alt="" />
-          <img style={imgStyle2} src="https://seeklogo.com/images/C/cpwd-logo-51F0BE53D4-seeklogo.com.png" alt="" />
-          <img style={imgStyle2} src="https://is1-2.housingcdn.com/0b8ad14c/c6c0511abac462ad907ad2afaebf7716/v1/medium.jpg" alt="" />
-          <img style={imgStyle2} src={b1} alt="brand pic" />
-          <img style={imgStyle2} src={b2} alt="brand pic" />
-          <img style={imgStyle2} src={b3} alt="brand pic" />
+          <img style={imgStyle} src="https://seeklogo.com/images/C/cpwd-logo-51F0BE53D4-seeklogo.com.png" alt="" />
+          <img style={imgStyle} src="https://is1-2.housingcdn.com/0b8ad14c/c6c0511abac462ad907ad2afaebf7716/v1/medium.jpg" alt="" />
+          <img style={imgStyle} src={b1} alt="brand pic" />
+          <img style={imgStyle} src={b2} alt="brand pic" />
+          <img style={imgStyle} src={b3} alt="brand pic" />
         </Marquee>
       </div>
     </>
