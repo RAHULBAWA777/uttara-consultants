94884c4200e4ba7120849989f21dc795129acddb
RAHULBAWA <rahulbawa514@gmail.com>
2023-06-11 14:43:49 +0530

    brands add

--- src/components/MARQUEE/Sponsors.jsx
+++ src/components/MARQUEE/Sponsors.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Marquee from "react-fast-marquee";
 import './sponsors.css'
+import b1 from '../../assets/brands/cropped-new-siddharth-developers-logo.png'
+import b2 from '../../assets/brands/image-233.jpg'
+import b3 from '../../assets/brands/image-332.png'
 const Sponsors = () => {
   
     const imgStyle = {
@@ -22,6 +25,9 @@ const Sponsors = () => {
           <img style={imgStyle} src="https://download.logo.wine/logo/Bharat_Sanchar_Nigam_Limited/Bharat_Sanchar_Nigam_Limited-Logo.wine.png" alt="" />
           <img style={imgStyle2} src="https://seeklogo.com/images/C/cpwd-logo-51F0BE53D4-seeklogo.com.png" alt="" />
           <img style={imgStyle2} src="https://is1-2.housingcdn.com/0b8ad14c/c6c0511abac462ad907ad2afaebf7716/v1/medium.jpg" alt="" />
+          <img style={imgStyle2} src={b1} alt="brand pic" />
+          <img style={imgStyle2} src={b2} alt="brand pic" />
+          <img style={imgStyle2} src={b3} alt="brand pic" />
         </Marquee>
       </div>
     </>
